3df7cc90c895cabad07eebeac7f559d98b75db8d
trunghieuntb <trunghieuntb@gmail.com>
2024-10-03 23:13:18 +0700

    init code

---
+++ CODE/Core/Inc/software_timer.h
@@ -0,0 +1,17 @@
+/*
+ * software_timer.h
+ *
+ *  Created on: Oct 3, 2024
+ *      Author: trung
+ */
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+
+extern int timer1_flag;
+
+void setTimer1(int duration);
+void timerRun();
+
+
+#endif /* INC_SOFTWARE_TIMER_H_ */
